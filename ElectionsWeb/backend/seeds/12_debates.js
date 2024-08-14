71e6ad090566617edd20b1e62a0c97bb7eaf7a87
mohammad282001 <32101001006@std.bau.edu.jo>
2024-08-14 23:36:54 +0300

    database issue

---
+++ ElectionsWeb/backend/seeds/12_debates.js
@@ -0,0 +1,13 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries in the 'payments' table
+  //   return knex("payments")
+  //     .del()
+  //     .then(function () {
+  //       // Inserts seed entries
+  //       return knex("payments").insert([
+  //         { id: 1, payment_type: "Credit Card", amount: 100.0 },
+  //         { id: 2, payment_type: "PayPal", amount: 200.0 },
+  //         { id: 3, payment_type: "Bank Transfer", amount: 300.0 },
+  //       ]);
+  //     });
+};
