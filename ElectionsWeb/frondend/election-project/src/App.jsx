dd81d6dd69d4231868aa38b1d591d5ddf8cf8778
mohammad282001 <32101001006@std.bau.edu.jo>
2024-08-14 11:25:50 +0300

    front end structure

--- ElectionsWeb/frondend/election-project/src/App.jsx
+++ ElectionsWeb/frondend/election-project/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Candidates from './pages/Candidates/candidates';
+import Home from './pages/Home/home';
+import AboutUs from './pages/AboutUs/aboutUs';
+import Login from './pages/Auth/Login/login';
+import ElectionLists from './pages/ElectionLists/electionLists';
+import Results from './pages/Results/results';
+import Voting from './pages/Voting/voting';
+import './App.css';
+import './index.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <p className="text-3xl font-bold underline">
-        Hello world!
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/candidates" element={<Candidates />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/electionLists" element={<ElectionLists />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/voting" element={<Voting />} />
+      </Routes>
+    </Router>
+  );
 }
-export default App
+
+export default App;
---
+++ ElectionsWeb/frondend/election-project/src/pages/Auth/Login/login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Login = () => {
+    return (
+        <>
+            <main>
+                <h1>components</h1>
+                {/* components */}
+                {/* components */}
+                {/* components */}
+            </main>
+        </>
+    );
+};
+export default Login;
